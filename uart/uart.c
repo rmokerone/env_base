4c1827e7cca35117b623bbe18a230156b3a0f73e
moker <rmokerone@gmail.com>
2013-11-09 15:49:08 +0800

    改写成头文件形式

--- uart/uart.c
+++ uart/uart.c
@@ -1,36 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
-
-#define BAUD 9600
-#define CRYSTAL 4000000
-
-#define BAUD_SETTING (unsigned int)((unsigned long)CRYSTAL/(16*(unsigned long)BAUD)-1)
-
-void init_uart(unsigned int baud);
-static int uart_putchar (char c, FILE * stream);
-unsigned char table[] = "hello world\n";
-volatile unsigned char data;
-
-static FILE mystdout = FDEV_SETUP_STREAM (uart_putchar, NULL, _FDEV_SETUP_WRITE);
-int main(void)
-{
-    DDRC = 0xff;
-    PORTC = 0X00;
-    init_uart (BAUD_SETTING);
-    sei ();
-    stdout = &mystdout;;
-    while(1)
-    {
-	if (data == 'a')
-	{
-	    PORTC = 0X00;
- 	    printf ("%s",table);
-	    data = '\0';
-	}
-    }
-    return 0;
-}
+#include "uart.h"
 
 void init_uart(unsigned int baud)
 {
@@ -43,7 +14,7 @@ void init_uart(unsigned int baud)
     UCSRC = (1 << URSEL)|(1 << UCSZ0)|(1<<UCSZ1);
 }
 
-static int uart_putchar (char c, FILE * stream)
+int uart_putchar (char c, FILE * stream)
 {
     if (c == '\n')
 	uart_putchar ('\r', stream);
@@ -58,7 +29,7 @@ ISR (USART_RXC_vect)
     status = UCSRA;
     if ((status&((1<<DOR)|(1<<FE)|(1<<PE)))==0)
     {
-	data = UDR;
+	uart_data = UDR;
 	PORTC = 0XFF;
     }
     else
